360842aa08c30f1470031653ab014b7955c240bf
1680X <101942111+1680X@users.noreply.github.com>
2022-03-22 23:51:59 +0200

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -62,7 +62,7 @@ pstart_markup = InlineKeyboardMarkup(
         ],
         [        
             InlineKeyboardButton(text=f"⏏️ | بوت السورس", url=f"https://t.me/Q_o_ll_bot"),
-            InlineKeyboardButton(text=f"🚸 | الــــمطور", url=f"https://t.me/Mahmo9"),
+            InlineKeyboardButton(text=f"🚸 | الــــمطور", url=f"https://t.me/Q_o_ll"),
         ],
         [
             InlineKeyboardButton(text=f"👥 | التواصل", url=f"https://t.me/{GROUP}"),
